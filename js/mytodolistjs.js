9e9147495e81826b279d8af02e428885e2437557
JS <sokolowskaju@gmail.com>
2018-07-09 19:33:15 +0200

    mytodolist

--- js/mytodolistjs.js
+++ js/mytodolistjs.js
@@ -6,14 +6,12 @@ list.addEventListener('click', function (ev) {
   if (ev.target.tagName === 'LI') {
     ev.target.classList.toggle('done');
   }
+  if(clikedLi.length === 8){
+    setTimeout(() => {
+      alert("BARDZO DZIĘKUJĘ MYSZKO!");
+    }, 200);
+  }
 }, false);
 
 
 
-  list.addEventListener('click', function (ev){
-if(clikedLi.length === 8){
-  setTimeout(() => {
-    alert("BARDZO DZIĘKUJĘ MYSZKO!");
-  }, 200);
-}
-});
