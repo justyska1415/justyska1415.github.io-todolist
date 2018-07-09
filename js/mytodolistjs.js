ef9d88689f8a6675d2fb90133cf2274e80495b63
JS <sokolowskaju@gmail.com>
2018-07-09 19:31:56 +0200

    lastchanges

--- js/mytodolistjs.js
+++ js/mytodolistjs.js
@@ -1,6 +1,19 @@
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if( ev.target.tagName === 'LI') {
-     ev.target.classList.toggle('done'); 
+let li = document.querySelector('li');
+let clikedLi = document.getElementsByClassName('done');
+let list = document.querySelector('ul');
+
+list.addEventListener('click', function (ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('done');
   }
 }, false);
+
+
+
+  list.addEventListener('click', function (ev){
+if(clikedLi.length === 8){
+  setTimeout(() => {
+    alert("BARDZO DZIĘKUJĘ MYSZKO!");
+  }, 200);
+}
+});
